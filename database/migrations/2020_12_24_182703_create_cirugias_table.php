39fa4ff5198cea39de494be949d00608938c19d8
YeffersonLinares <ylinares@unbosque.edu.co>
2020-12-25 19:45:53 -0500

    migraciones terminadas

--- database/migrations/2020_12_24_182703_create_cirugias_table.php
+++ database/migrations/2020_12_24_182703_create_cirugias_table.php
@@ -16,11 +16,15 @@ class CreateCirugiasTable extends Migration
         Schema::create('cirugias', function (Blueprint $table) {
             $table->increments('cod_cirugia');
             $table->string('nom_cirugia')->nullable($value = true);
-            $table->integer('cod_t_cirugia')->nullable($value = true);
-            $table->integer('cod_paciente')->nullable($value = true);
-            $table->integer('cod_sitio')->nullable($value = true);
+            $table->integer('cod_t_cirugia')->unsigned()->nullable($value = true);
+            $table->integer('cod_paciente')->unsigned()->nullable($value = true);
+            $table->integer('cod_sitio')->unsigned()->nullable($value = true);
             $table->date('fecha_cirugia')->nullable($value = true);
             $table->timestamps();
+
+            $table->foreign('cod_t_cirugia')->references('cod_t_cirugia')->on('tipo_cirugias');
+            $table->foreign('cod_paciente')->references('cod_paciente')->on('pacientes');
+            $table->foreign('cod_sitio')->references('cod_sitio')->on('sitios');
         });
     }
 
